1c37c113517155a835aa70572fac92da0f27cfed
Klaus.Guo <ghdny2015@outlook.com>
2026-05-06 10:33:04 +0800

    feat: change visibility of FunKV methods from private to public

--- src/core/internal.rs
+++ src/core/internal.rs
@@ -10,35 +10,35 @@ use crate::{
 use super::FunKV;
 
 impl FunKV {
-    pub(super) fn builder() -> DbBuilder {
+    pub fn builder() -> DbBuilder {
         DbBuilder::new()
     }
 
-    pub(super) fn contains_key(&self, key: &[u8]) -> bool {
+    pub fn contains_key(&self, key: &[u8]) -> bool {
         self.hash_table.contains_sync(key)
     }
 
-    pub(super) fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.stats
             .record_count
             .load(std::sync::atomic::Ordering::Acquire) as usize
     }
 
-    pub(super) fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
 
-    pub(super) fn memory_usage(&self) -> usize {
+    pub fn memory_usage(&self) -> usize {
         self.stats
             .memory_usage
             .load(std::sync::atomic::Ordering::Acquire)
     }
 
-    pub(super) fn stats(&self) -> StatsSnapshot {
+    pub fn stats(&self) -> StatsSnapshot {
         self.stats.snapshot()
     }
 
-    pub(super) fn flush(&self) {
+    pub fn flush(&self) {
         self.flush_all()
     }
 
